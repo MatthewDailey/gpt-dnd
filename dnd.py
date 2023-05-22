9c09fddff43376ce4e90df0396f25884a00fd8d2
Matt Dailey <mjd@figma.com>
2023-05-21 18:59:50 -0700

    add print + speak capability

--- dnd.py
+++ dnd.py
@@ -3,8 +3,10 @@ import os
 import time
 import argparse
 import joblib
+import gtts
+from playsound import playsound
 
-memory = joblib.Memory(location=".cached_data", verbose=1)
+memory = joblib.Memory(location=".cached_data", verbose=0)
 SEPARATOR = "==SEP=="
 
 openai.organization = "org-3676qVMg5QssbgHBYPtoL1DT"
@@ -24,10 +26,7 @@ def openai_request(messages, model, temperature, cache_buster=None):
     return result
 
 
-def main(args):
-    if "PERSONAL_OPENAI_API_KEY" not in os.environ:
-        raise ValueError("OPENAI_API_KEY not set")
-
+def send_prompts(args):
     with open(args.dir + "/prompt.txt") as f:
         prompts = f.read().split(SEPARATOR)
 
@@ -61,8 +60,33 @@ def main(args):
         with open(os.path.join(out_dir, f"result_{i}.txt"), "w") as f:
             f.write(response_message["content"])
 
-        print(response_message)
         messages.append(result["choices"][0]["message"])
+
+    return messages[-1]["content"]
+
+
+def get_input_and_write_to_prompt(args):
+    user_action = input("\n\n")
+    with open(args.dir + "/prompt.txt", "a") as f:
+        f.write(SEPARATOR + "\n\n" + user_action)
+
+
+def speak_and_print(dir, s):
+    tts = gtts.gTTS(s, lang="en-uk")
+    tts.save(dir + "current.mp3")
+    playsound(dir + "current.mp3")
+    print(s)
+
+
+def main(args):
+    if "PERSONAL_OPENAI_API_KEY" not in os.environ:
+        raise ValueError("OPENAI_API_KEY not set")
+
+    speak_and_print(args.dir, send_prompts(args))
+
+    while args.continuous:
+        get_input_and_write_to_prompt(args)
+        speak_and_print(args.dir, send_prompts(args))
 
 
 if __name__ == "__main__":
@@ -86,6 +110,13 @@ if __name__ == "__main__":
         type=bool,
         default=False,
         help="Skip the cache and make a new request",
+    )
+    parser.add_argument(
+        "-c",
+        "--continuous",
+        type=bool,
+        default=False,
+        help="Run continuously, waiting for user input",
     )
 
     main(parser.parse_args())
