6947abbe7cdcbffd70a13edd686848fc3ece22ea
Matt Dailey <mjd@figma.com>
2023-05-29 17:59:18 -0700

    add music and better example with pygame

--- test_youtube_audio.py
+++ test_youtube_audio.py
@@ -1,13 +1,17 @@
-import pygame
+import os
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+import pygame  # noqa: E402
 
 pygame.init()
 pygame.mixer.init()
-pygame.mixer.music.load("experiments/debugging/current.mp3")
-pygame.mixer.music.set_volume(0.2)
+# pygame.mixer.music.load("experiments/debugging/current.mp3")
+pygame.mixer.music.load("the-adventure-begins.mp3")
+pygame.mixer.music.set_volume(0.8)
 pygame.mixer.music.play()
 
-while pygame.mixer.music.get_busy():
-    pygame.time.Clock().tick(10)
+while True:
+    pass
 
 # sound.set_volume(0.9)  # Now plays at 90% of full volume.
 # sound.play()
